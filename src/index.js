fcc47455452ceaa2b48c6adf5b16f39477e6179c
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2021-09-04 23:20:54 -0400

    add bootstrap, scss deps to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// REMOVED to use SASS/SCSS
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/newthing.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap';
+// @import 'custom-style';
+
+const Th = () => {
+    return (
+        <Button>fjdksl;jhkl</Button>
+    )
+
+}
+
+export default Th;
